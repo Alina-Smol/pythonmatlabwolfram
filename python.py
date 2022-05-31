523b9cfd41d7255c8d919241e7921b839c709be4
Alina-Smol <78797567+Alina-Smol@users.noreply.github.com>
2022-05-31 21:24:43 +0300

    Update python.py

--- python.py
+++ python.py
@@ -221,4 +221,123 @@ dff = pd.DataFrame(data = df, columns = ["normal", "random", "gamma"])
 sns.boxplot(x = None, y = None, data = dff)
 bp = sns.boxplot(x = "variable", y = 'value', data = pd.melt(dff), whis = [0, 100])
 
+#########################
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+ax = axes3d.Axes3D(plt.figure())
+i = np.arange(-1,1,0.01)
+X,Y=np.meshgrid(i,i)
+Z = X**2 + Y**3
+ax.plot_wireframe(X,Y,Z,rstride=3,cstride=3)
+plt.show()
+
+#############################
+from matplotlib import projections
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as pit
+fig = pit.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+####################
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+x = y = np.linspace(-3,3,74)
+X,Y=np.meshgrid(x,y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(4*R)/R
+fig, ax = plt.subplots(1,3,figsize=(14,4), subplot_kw=dict(projection = '3d'))
+
+norm = mpl.colors.Normalize(-abs(Z).max(), abs(Z).max())
+p=ax[0].plot_surface(X,Y,Z,rstride=1,cstride=1,linewidth=0,
+antialiased = True, norm = norm, cmap=mpl.cm.Blues)
+cb = fig.colorbar(p, ax=ax[0], shrink=0.6)
+ax[0].set_xlabel("$x$", fontsize = 16)
+ax[0].set_ylabel("$y$", fontsize = 16)
+ax[0].set_zlabel("$z$", fontsize = 16)
+
+ax[1].plot_wireframe(X,Y,Z,rstride=3,cstride=3, color = "yellow")
+ax[1].set_title("plot_wireframe")
+
+ax[2].contour(X,Y,Z,zdir="z",offset=0, norm=norm,cmap=mpl.cm.jet)
+ax[2].contour(X,Y,Z,zdir="y",offset=3, norm=norm,cmap=mpl.cm.Blues)
+plt.show()
+
+#######################################
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import matplotlib.pyplot as mpl
+from mpl_toolkits.mplot3d import axes3d
+
+
+ax[1].plot_wireframe(X,Y,Z, rstride = 3, cstride = 3, color = "darkgrey")
+ax[1].set_title("plot_wireframe")
+
+import seaborn as sns
+np.random.seed(1234)
+
+v1 = pd.Series(np.random.normal(0,10,5000), name = 'v1')
+v2 = pd.Series(2*v1+np.random.normal(60,15,5000),name = 'v2')
+
+figure = mpl.figure()
+mpl.hist(v1,alpha = 0.7, bins = np.arange(-50,150,5), label = 'v1')
+mpl.hist(v2,alpha = 0.7, bins = np.arange(-50,150,5), label = 'v1')
+
+from locale import normalize
+mpl.figure()
+mpl.hist([v1,v2], histtype="barstacked", density = True)
+v3 = np.concatenate((v1, v2))
+sns.kdeplot(v3)
+
+mpl.figure()
+sns.distplot(v3,hist_kws = {'color':'Teal'}, kde_kws ={'color':'Navy'})
+
+mpl.figure()
+sns.jointplot(v1,v2,alpha = 0.5, kind = "hex")
+
+
+###############
+from math import *
+
+def temperature(t: float) -> float:
+  return (atan(-0.0012*t**3+0.4*t**2+0.616*t+6120)+0.65*sin(0.24*t+1.23)-0.27*cos(0.21*t-0.17)-(sin(0.34*t+0.16))/(1+0.03*(t-370.5)**2))
+
+x = range(0, 1000)
+y = [temperature(t) for t in x]
+
+
+plt.plot(x,y)
+plt.show()
+
+###########
+from collections import Counter
+import math
+with open('orwell_1984.txt', encoding = 'utf8') as text:
+  words_lenghts_counter = Counter(len(word) for word in text.read().split())
+
+words_lenghts_counter
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.set_xlim([0,30])
+ax.set_ylim([0,40000])
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+longestWordLength = max(words_lenghts_counter)
+
+histogram = [words_lenghts_counter[wordLen] for wordLen in range(1, longestWordLength + 1)]
+
+plt.hist(list(range(1, longestWordLength + 1)), weights = histogram, bins=longestWordLength+1, orientation='vertical')
+
+######################
 
