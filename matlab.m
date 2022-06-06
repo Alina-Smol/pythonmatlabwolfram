5af1677f600e635763fe620d9917645f49bb34a6
Alina-Smol <78797567+Alina-Smol@users.noreply.github.com>
2022-06-06 10:06:44 +0300

    Update matlab.m

--- matlab.m
+++ matlab.m
@@ -81,15 +81,15 @@ fsurf(fx,fy,fz, [-pi,pi,-pi,pi])
 %числ.методы%
 %деление пополам
 clear
-a = 0; b = 14;
-x = linspace(a,b,500)
+a = 0; b = 14; m = 500;
+x = linspace(a,b,m)
 h = 0.001; iter=1000; eps=0.001;
 f=@(x)sin(x)./x;
 y=f(x);
 plot(x,f(x),x,0*x,':'); grid on
 xlabel('x'); ylabel('y')
 hold on
-ymin=min(y); ymax=max(y);
+ymin=min(y); ymax=max(y); %задаем размеры осей
 if ymin<0 ymin=1.1*ymin; else ymin=0.9*ymin; end;
 if ymax>0 ymax=1.1*ymax; else ymax=0.9*ymax; end;
 axis([a,b,ymin,ymax]);
@@ -128,7 +128,7 @@ plot(x,f(x),x,0*x,':');
 grid on
 xlabel('x'); ylabel('y')
 hold on
-ymin=min(y); ymax=max(y);
+ymin=min(y); ymax=max(y);%задаются оси
 if ymin<0 ymin=1.1*ymin; else ymin = 0.9*ymin; end;
 if ymax>0 ymax=1.1*ymax; else ymax = 0.9*ymax; end;
 ylim([ymin,ymax]);
